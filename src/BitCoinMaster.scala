db134a9c0768e5d3bf99020181510931435b5e35
Balaji Iyer <balaji-iyer>
2014-09-16 12:51:17 -0400

    Resolved conflicts

--- src/BitCoinMaster.scala
+++ src/BitCoinMaster.scala
@@ -16,11 +16,19 @@ object BitCoinMaster {
     val userName = "COP5615 is a boring clas"
     val sha1 = generateSHA256Token(userName);
     println( "SHA1: " + sha1 )
-      
+     
+    //Create a actor system
+    val system = ActorSystem("MasterSystem")
+    val remoteActor = system.actorOf(Props[MiningActor], name = "RemoteActor1")
+    remoteActor ! "The RemoteActor is alive"
+    
+    
+    
+    
     
 }
 
-class LocalActor extends Actor{
+class MiningActor extends Actor{
   val remote = context.actorSelection("akka.tcp://HelloRemoteSystem@10.136.79.71:2552/user/RemoteActor")
   var counter=0
   def receive ={
