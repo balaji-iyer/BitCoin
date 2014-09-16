116e3ae02aa51c6806ffbd99559ec236a0d242b0
Balaji Iyer <balaji-iyer>
2014-09-16 12:11:21 -0400

    First commit with sha256

---
+++ src/BitCoinMaster.scala
@@ -0,0 +1,38 @@
+/**
+ * Code to generate SHA256 hash
+ * Input : String
+ */
+
+import BearerTokenGenerator._
+import akka.actor._
+import com.typesafe.config.ConfigFactory
+
+object BitCoinMaster {
+
+  def main(args: Array[String]): Unit = {}
+  	/**
+  	 * Test code for a particular string
+  	 */
+    val userName = "COP5615 is a boring clas"
+    val sha1 = generateSHA256Token(userName);
+    println( "SHA1: " + sha1 )
+      
+    
+}
+
+class LocalActor extends Actor{
+  val remote = context.actorSelection("akka.tcp://HelloRemoteSystem@10.136.79.71:2552/user/RemoteActor")
+  var counter=0
+  def receive ={
+    case "START" =>
+      		remote ! "Hello from LocalActor"
+    case msg : String=>
+      		println(s"LocalActor received message:'$msg'");
+      		if(counter<5){
+      			sender ! "Hello back to you"
+      			counter += 1
+      		}
+  }
+  
+}
+
