04ecfddaead00fc180446970d0cd7fa619276e45
Balaji Iyer <balaji-iyer>
2014-09-18 18:22:08 -0400

    Add: Throughput calculation and tracking actors

--- README.md
+++ README.md
@@ -1,5 +1,55 @@
 BitCoin
 =======
-
-
 The goal is mine bitcoins using akka actor model so that it runs well on multiple core machines.
+
+Prerequisites:
+=======
+Scala 2.11.2
+Akka 2.3.6 distribution for Scala 2.11
+
+Instructions to execute:
+=======
+Compile:
+	scalac -cp  "../lib/scala-library-2.11.2.jar:../lib/akka/*:." BitCoinMaster.scala
+
+Run:
+	Server Mode: scala -cp  "../lib/scala-library-2.11.2.jar:../lib/akka/*:." BitCoinMaster <number of zeros>
+	Client mode: scala -cp  "../lib/scala-library-2.11.2.jar:../lib/akka/*:." BitCoinMaster <server ip>
+
+Observations:
+=======
+
+1. Size of the work unit:
+
+
+
+2. For : scala project1.scala 4
+	Result file: 4_zeroes.txt
+	actorCount is 8
+	Total Number of bitcoins mined is 586
+	Total number of messages processed is 37700000
+	Throughtput is 26.71716296453235 messages/sec
+	2095.64user 24.28system 5:04.34elapsed 696%CPU (0avgtext+0avgdata 228476maxresident)k
+	0inputs+976outputs (0major+74386minor)pagefaults 0swaps
+
+   For : scala project1.scala 5
+    Result file: 5_zeroes.txt
+    actorCount is 8
+	Total Number of bitcoins mined is 27
+	Total number of messages processed is 37000000
+	Throughtput is 26.221078732160073 messages/sec
+	2000.01user 26.43system 5:04.64elapsed 665%CPU (0avgtext+0avgdata 243052maxresident)k
+	0inputs+608outputs (0major+93682minor)pagefaults 0swaps
+
+
+
+
+3. The coin with the most 0s :
+
+4. The largest number of working machines you were able to run your code with : 12
+
+
+
+
+
+
--- src/BitCoinMaster.scala
+++ src/BitCoinMaster.scala
@@ -1,13 +1,16 @@
 /**
- * Code to generate SHA256 hash
- * Input : String
+ * Code to mine bitcoins
+ * 
  */
 
-import BearerTokenGenerator._
+//import BearerTokenGenerator._
 import akka.actor._
 import com.typesafe.config.ConfigFactory
 import scala.actors.Actor.self
 import scala.actors.TIMEOUT
+import scala.actors.threadpool.TimeUnit
+import java.security.SecureRandom
+import java.security.MessageDigest
 
 
 case object execute
@@ -15,13 +18,16 @@ case object interrupt
 case object done
 case class bitcoin(input :String,output : String)
 case object terminate
-
+case class terminate(duration: Long)
 
 case object rmsg
 case object msg
 case object Test
 case object sendzeroes
 case object getNoOfZeroes
+
+case object sendActorCount
+case object actorcount
 
 object BitCoinMaster{
    def main(args: Array[String]): Unit = { 
@@ -44,27 +50,28 @@ object BitCoinMaster{
   if(remoteHost == "127.0.0.1"){
     val masterSystem = ActorSystem("MasterSystem", ConfigFactory.load("Master.conf"))
     val master=masterSystem.actorOf(Props(new Master(zeroesExpected,remoteHost)),name="Master") 
-    val processors = Runtime.getRuntime().availableProcessors()+Runtime.getRuntime().availableProcessors()
-   
-    //val actor1 = masterSystem.actorOf(Props(new MiningActor(remoteHost,master)),name="Test")
-    //actor1! execute(zeroesExpected)
-  
-    for(i<- 0 to processors){
+    val processors = Runtime.getRuntime().availableProcessors()
+    
+    for(i<- 0 to processors-1){
+    	
     	val actorName="MiningActor"+i.toString()
     	var actor = masterSystem.actorOf(Props(new MiningActor(remoteHost,master,zeroesExpected)), name = actorName)
+    	actor!sendActorCount
     	actor! execute     
-}
+    }
+    
+    //Start timer
     val timer = masterSystem.actorOf(Props(new Timer(master)), "timer")
    }
     else{
     val localSystem = ActorSystem("LocalSystem",ConfigFactory.load("Local.conf"))
-    val processors = Runtime.getRuntime().availableProcessors()+Runtime.getRuntime().availableProcessors()
-    for(i<- 0 to processors){
+    val processors = Runtime.getRuntime().availableProcessors()
+    for(i<- 0 to processors-1){
         val actorName="MiningActor"+i.toString()
         var actor = localSystem.actorOf(Props(new RemoteMiningActor(remoteHost)), name = actorName)
+        actor!sendActorCount
         actor ! getNoOfZeroes
-        //actor! execute
- 
+         
 } 
    
   } 
@@ -79,38 +86,59 @@ class Master(zeroesExpected: Int, remoteHost:String) extends Actor {
 var hashMap = Map.empty[String, String]
 var totalMessages=0
 var flag=false
-var actorCount=8
+var actorCount=0
+var totalDuration: Long=0
+var actorNo=0
+var bitcoinCount=0
+
 def receive={
 case `interrupt` =>
       {
     	  if(flag==false){  
-    		  totalMessages+=1;
+    		  totalMessages+=100000;
     		  sender ! execute
     	  }
     	  else
     	  {
-    		  totalMessages+=1;
+    		  totalMessages+=100000;
     		  sender ! terminate
     	  }
      
       }
     case bitcoin(input,output) =>
       {
+       println("bitcoin:"+input+"\thash:"+ output)
+       bitcoinCount+=1
       //println("Hello from MiningActor")
       hashMap+= (input->output)
       }
      
     case `done` => 
-      				{ actorCount+=1
-      				  //if(actorCount==10)
-      				 println(totalMessages)
+      				{ 
+      				  actorNo+=1
+      				 //val throughtput = (totalMessages * 1000.0 / totalDuration).toInt
+      				 //println(s"== It took $totalDuration secs to deliver $totalMessages messages, throughtput $throughtput msg/s, " +
+      						 	//s"payload size 10000")	
       				 //Thread.sleep(100)
-      				 context.system.shutdown()
+      				  if(actorNo==actorCount){
+      				    println(s"actorCount is $actorCount")
+      				    println(s"Total Number of bitcoins mined is $bitcoinCount")
+      				    println(s"Total number of messages processed is $totalMessages")
+      				    //println(totalDuration)
+      				    val throughtput = (totalMessages * 1000.0 / TimeUnit.MILLISECONDS.toSeconds(totalDuration))
+      				    println(s"Throughtput is $throughtput messages/sec")
+      				    context.system.shutdown()
+      				  }
       				}
         
-    case `terminate` => flag=true
+    case terminate(duration) => {
+    								totalDuration=duration
+    								flag=true
+    							}	
     
-    case sendzeroes => sender ! zeroesExpected
+    case `sendzeroes` => sender ! zeroesExpected
+    
+    case actorcount=> actorCount+=1
          
   }
 }
@@ -121,19 +149,24 @@ case `interrupt` =>
    
   var flag = true 
   var messageCount=0
-  
+  var hashMap = Map.empty[String, String]
   var counter=0
+ 
   def receive ={
+    
+      case `sendActorCount`=> masterRef!actorcount   
       case `execute` =>
         {
-      //for(i<- 0 to 10000000){
+        			for(i<- 0 to 100000){
         			val input = BearerTokenGenerator.getToken()
+        			
         			val hashOutput = BearerTokenGenerator.generateSHAToken(input)
         			if (Common.isBitCoin(hashOutput,z)){
+        				//hashMap+= (input->hashOutput)
         				masterRef! bitcoin(input,hashOutput)
-        				println("bitcoin:"+input+"\thash:"+ hashOutput)
+        				//println("bitcoin:"+input+"\thash:"+ hashOutput)
         			}
-     
+        		}
         			masterRef ! interrupt
         }
      
@@ -162,16 +195,18 @@ case `interrupt` =>
   var counter=0
   var zeroes=0
   def receive ={
+    
+	  case `sendActorCount`=> masterRef!actorcount
       case `execute` =>
         {
-      //for(i<- 0 to 10000000){
+        			for(i<- 0 to 100000){
         			val input = BearerTokenGenerator.getToken()
         			val hashOutput = BearerTokenGenerator.generateSHAToken(input)
-        			//println("The real zeroes",zeroes)
         			if (Common.isBitCoin(hashOutput,zeroes)){
         				masterRef! bitcoin(input,hashOutput)
-        				println("bitcoin:"+input+"\thash:"+ hashOutput)
+        			
         			}
+        		}
         			masterRef ! interrupt
         }
      
@@ -181,10 +216,10 @@ case `interrupt` =>
       sender ! rmsg
      
     case `terminate` =>
-      {masterRef ! done
-       context.stop(self)
-      //context.system.shutdown()
-      }
+      			{	masterRef ! done
+      				context.stop(self)
+      //context.system.shutdown())))
+      			}
    
     case `getNoOfZeroes` => masterRef ! sendzeroes        
     
@@ -204,11 +239,10 @@ case `interrupt` =>
 	{
 	  var currentTime = System.currentTimeMillis()
 	  
-	  if(currentTime >= startTime+60000)
+	  if(currentTime >= startTime+300000)
 	  {
-	    println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
 	    flag=false
-	    masterRef ! terminate
+	    masterRef ! terminate(currentTime)
 	  }
 	}
 	def receive={
@@ -216,3 +250,84 @@ case `interrupt` =>
 	}
 	
 }
+ 
+ object BearerTokenGenerator {
+  
+	val TOKEN_LENGTH = 45	// TOKEN_LENGTH is not the return size from a hash, 
+				// but the total characters used as random token prior to hash
+				// 45 was selected because System.nanoTime().toString returns
+				// 19 characters.  45 + 19 = 64.  Therefore we are guaranteed
+				// at least 64 characters (bytes) to use in hash, to avoid MD5 collision < 64
+	val TOKEN_CHARS = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz_.-"
+	val secureRandom = new SecureRandom()
+
+	private def toHex(bytes: Array[Byte]): String = bytes.map( "%02x".format(_) ).mkString("")
+
+	private def sha(s: String): String = { 
+	    toHex(MessageDigest.getInstance("SHA-256").digest(s.getBytes("UTF-8")))
+	}
+	private def md5(s: String): String = { 
+	    toHex(MessageDigest.getInstance("MD5").digest(s.getBytes("UTF-8")))
+	}
+
+	// use tail recursion, functional style to build string.
+	private def generateToken(tokenLength: Int) : String = {
+		val charLen = TOKEN_CHARS.length()
+   		def generateTokenAccumulator(accumulator: String, number: Int) : String = {
+	       	if (number == 0) return accumulator
+	       	else
+	           generateTokenAccumulator(accumulator + TOKEN_CHARS(secureRandom.nextInt(charLen)).toString, number - 1)
+	   	}
+   		generateTokenAccumulator("", tokenLength)
+	}
+
+	/*
+	 *  Hash the Token to return a 32 or 64 character HEX String
+	 *  
+	 *  Parameters:
+	 *  tokenprifix: string to concatenate with random generated token prior to HASH to improve uniqueness, such as username
+     *
+     *  Returns:
+     *  MD5 hash of (username + current time + random token generator) as token, 128 bits, 32 characters
+     * or
+     *  SHA-256 hash of (username + current time + random token generator) as token, 256 bits, 64 characters
+     */
+	def generateMD5Token(tokenprefix: String): String =  {
+		md5(tokenprefix + System.nanoTime() + generateToken(TOKEN_LENGTH)) 
+	}
+	/*def generateSHAToken(tokenprefix: String): String =  {
+		sha(tokenprefix + System.nanoTime() + generateToken(TOKEN_LENGTH)) 
+	}*/
+	//getInput String
+	def getToken(tokenprefix: String): String =  {
+		return(tokenprefix + System.nanoTime() + generateToken(TOKEN_LENGTH)) 
+	}
+	def generateSHA256Token(tokenprefix: String): String =  {
+		sha(tokenprefix) 
+	}
+	// final 2 methods we're using
+	def getToken(): String =  {
+		return("balajiiyer" + System.nanoTime()) 
+	}
+	
+	def generateSHAToken(shaInput :String):String ={
+	  sha(shaInput)
+	}
+}
+
+object Common {
+	def isBitCoin(hashValue: String , zeroesExpected : Int):Boolean = {
+		var flag = true
+		for (index <- 0 to (zeroesExpected-1))
+	    {
+	    		  if(hashValue.charAt(index)!='0')
+	    		  {
+	    		    flag= false
+	    		    return flag
+	    		  }
+	    		  
+	    }
+	    	  return flag
+		}
+}
+
