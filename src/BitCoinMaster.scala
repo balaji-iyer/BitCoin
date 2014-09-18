f7bc1144035488da12ff7687a0d6f55b2cafbbb4
Balaji Iyer <balaji-iyer>
2014-09-17 22:11:13 -0400

    First Working Version
    Remote and Local execution.Need to work on performance issues

--- src/BitCoinMaster.scala
+++ src/BitCoinMaster.scala
@@ -6,204 +6,213 @@
 import BearerTokenGenerator._
 import akka.actor._
 import com.typesafe.config.ConfigFactory
+import scala.actors.Actor.self
+import scala.actors.TIMEOUT
 
 
-case class start(zeroes :Int)
+case object execute
+case object interrupt
+case object done
+case class bitcoin(input :String,output : String)
+case object terminate
 
-case object start1
+
+case object rmsg
+case object msg
+case object Test
+case object sendzeroes
+case object getNoOfZeroes
 
 object BitCoinMaster{
    def main(args: Array[String]): Unit = { 
   //Accept number of zeroes and ip address arguments from command line
-  val zeroesExpected = args(0).toInt
-  val remoteHostPort = if (args.length >= 2) args(1) else "127.0.0.1:2552"
-    
-   if(remoteHostPort == "127.0.0.1:2552"){
-    val remoteHostPort = "10.136.14.46:2552"
-    val remotePath = s"akka.tcp://MasterSystem@$remoteHostPort/user/MiningActor" 
-    val system = ActorSystem("MasterSystem", ConfigFactory.load("Master.conf"))
-    val actor=system.actorOf(Props[MiningActor],
-     name = "MiningActor")
-     //actor!"START"
+  
+  var param =  args(0)
+  var remoteHost=""
+  var zeroesExpected=0
+  if(param.toString().contains('.')){
+ remoteHost = param.toString()  
+  }
+  else{
+ zeroesExpected = args(0).toInt
+ remoteHost = "127.0.0.1" 
+  }
+ 
+  
+ 
+  //2nd method
+  if(remoteHost == "127.0.0.1"){
+    val masterSystem = ActorSystem("MasterSystem", ConfigFactory.load("Master.conf"))
+    val master=masterSystem.actorOf(Props(new Master(zeroesExpected,remoteHost)),name="Master") 
+    val processors = Runtime.getRuntime().availableProcessors()+Runtime.getRuntime().availableProcessors()
    
-  //system.actorOf(BitCoinMaster.mprops(remotePath, zeroesExpected, remoteHostPort), "MiningActor")
+    //val actor1 = masterSystem.actorOf(Props(new MiningActor(remoteHost,master)),name="Test")
+    //actor1! execute(zeroesExpected)
+  
+    for(i<- 0 to processors){
+    	val actorName="MiningActor"+i.toString()
+    	var actor = masterSystem.actorOf(Props(new MiningActor(remoteHost,master,zeroesExpected)), name = actorName)
+    	actor! execute     
+}
+    val timer = masterSystem.actorOf(Props(new Timer(master)), "timer")
    }
-   else{
-     val remoteHostPort = "10.136.14.46:2552"
-     val remotePath = s"akka.tcp://MasterSystem@$remoteHostPort/user/RemoteMiningActor"
-     val system = ActorSystem("LocalSystem", ConfigFactory.load("Local.conf"))
+    else{
+    val localSystem = ActorSystem("LocalSystem",ConfigFactory.load("Local.conf"))
+    val processors = Runtime.getRuntime().availableProcessors()+Runtime.getRuntime().availableProcessors()
+    for(i<- 0 to processors){
+        val actorName="MiningActor"+i.toString()
+        var actor = localSystem.actorOf(Props(new RemoteMiningActor(remoteHost)), name = actorName)
+        actor ! getNoOfZeroes
+        //actor! execute
+ 
+} 
+   
+  } 
+  
+  } 
+   
+   
+}
+
+class Master(zeroesExpected: Int, remoteHost:String) extends Actor {
+
+var hashMap = Map.empty[String, String]
+var totalMessages=0
+var flag=false
+var actorCount=8
+def receive={
+case `interrupt` =>
+      {
+    	  if(flag==false){  
+    		  totalMessages+=1;
+    		  sender ! execute
+    	  }
+    	  else
+    	  {
+    		  totalMessages+=1;
+    		  sender ! terminate
+    	  }
      
-     val actor=system.actorOf(Props[RemoteMiningActor],
-      name = "RemoteMiningActor")
-     actor!"START"
-     //system.actorOf(BitCoinMaster.sprops(remotePath, zeroesExpected, remoteHostPort), "RemoteMiningActor")
-   }
-   Thread.sleep(100000)
-   }
-
-  def mprops(path: String, zeroesExpected: Int, remoteHostPort: String): Props =
-    Props(classOf[MBitCoin],path, zeroesExpected, remoteHostPort)
-  def sprops(path: String, zeroesExpected: Int, remoteHostPort: String): Props =
-    Props(classOf[SBitCoin],path, zeroesExpected, remoteHostPort)
-   
+      }
+    case bitcoin(input,output) =>
+      {
+      //println("Hello from MiningActor")
+      hashMap+= (input->output)
+      }
+     
+    case `done` => 
+      				{ actorCount+=1
+      				  //if(actorCount==10)
+      				 println(totalMessages)
+      				 //Thread.sleep(100)
+      				 context.system.shutdown()
+      				}
+        
+    case `terminate` => flag=true
+    
+    case sendzeroes => sender ! zeroesExpected
+         
+  }
 }
+
   
-  /*if(remoteHostPort == "127.0.0.1:2552")
-		{
-			val processors = Runtime.getRuntime().availableProcessors()
-			val MiningActor1 = system.actorOf(Props[LocalMiningActor], name = "MiningActor1")
-			val MiningActor2 = system.actorOf(Props[LocalMiningActor], name = "MiningActor2")
-			val MiningActor3 = system.actorOf(Props[LocalMiningActor], name = "MiningActor3")
-			val MiningActor4 = system.actorOf(Props[LocalMiningActor], name = "MiningActor4")
-			MiningActor1 ! Start(zeroesExpected)
-			MiningActor2 ! Start(zeroesExpected)
-			MiningActor3 ! Start(zeroesExpected)
-			MiningActor4 ! Start(zeroesExpected)
-		}
-		*/
-
-
-class MBitCoin(path: String, zeroesExpected: Int, remoteHostPort:String) extends Actor {
-
+  
+ class MiningActor(remoteHost : String, masterRef : ActorRef,z : Int) extends Actor{
    
-  def receive={
-    case "RECEIVE" =>
-     		println("Hello from MiningActor")
-  }
-    //Create a actor system
-    //val systemTest = ActorSystem("System", ConfigFactory.load("Local.conf"))    
-    
-  	if(remoteHostPort == "127.0.0.1:2552")
-		{
-  	  /*
-			val processors = Runtime.getRuntime().availableProcessors()+4
-			
-			for(i<- 0 to processors){
-			  val actorName="MiningActor"+i.toString()
-			  var actor = system.actorOf(Props[MiningActor], name = actorName)
-			  actor! start(zeroesExpected)
-			  
-			}
-			*/
-  	  //println("Trying just remote")
-  	  //val remote = context.actorSelection(path)
-		//	remote ! "START"
-  	  
-  		println("Local")
-//  		println("Trying just remote")
-  		println(path)
-		//val remote = context.actorSelection(s"akka.tcp://MasterSystem@10.136.14.46:2552/user/Mining")
-		//remote ! "START"
-  	  
-		}
-//  		else{
-//  		  println("Trying just remote")
-//  		  println(path)
-//		  val remote = context.actorSelection(path)
-//		  remote ! "START"
-//  		}
-			    
-    		//self! "RECEIVE"
-}
-
-class SBitCoin(path: String, zeroesExpected: Int, remoteHostPort:String) extends Actor {
-
-   
-  def receive={
-    case "RECEIVE" =>
-     		println("Hello from MiningActor")
-  }
-    //Create a actor system
-    //val systemTest = ActorSystem("System", ConfigFactory.load("Local.conf"))    
-    
-  	if(remoteHostPort != "127.0.0.1:2552")
-		{
-  	  /*
-			val processors = Runtime.getRuntime().availableProcessors()+4
-			
-			for(i<- 0 to processors){
-			  val actorName="MiningActor"+i.toString()
-			  var actor = system.actorOf(Props[MiningActor], name = actorName)
-			  actor! start(zeroesExpected)
-			  
-			}
-			*/
-  	  //println("Trying just remote")
-  	  //val remote = context.actorSelection(path)
-		//	remote ! "START"
-  	  
-  		
-  		println("Trying just remote")
-  		println(path)
-		val remote = context.actorSelection(path)
-		remote ! "START"
-  	  
-		}
-  	else{
-  		 println("Trying just remote")
-  		 println(path)
-		 val remote = context.actorSelection(path)
-		 remote ! "START"
-  		}
-			    
-    		//self! "RECEIVE"
-}
-
-
-class MiningActor extends Actor{
-  //val remote = context.actorSelection("akka.tcp://MasterSystem@"+ip+":2552/user/RemoteMiningActor")
+  var flag = true 
+  var messageCount=0
+  
   var counter=0
   def receive ={
-    case "start1" =>
-      		println("Remote pinged me")
-    case msg : String=>
-      		println(s"LocalActor received message:'$msg'");
-      		if(counter<5){
-      			sender ! "Hello back to you"
-      			counter += 1
-      		}
-    case z : Int =>
-	    {
-	      for(i<- 0 to 10000000)
-	      {
-	    	  var flag = true
-	    	  val input = BearerTokenGenerator.getToken()
-	    	  val hashOutput = BearerTokenGenerator.generateSHAToken(input)
-	    	  if (Common.isBitCoin(hashOutput,z))
-	    		  println("bitcoin:"+input+"\thash:"+ hashOutput)
-	      }
-	      
-	    }		
-  }
-}
-  
- class RemoteMiningActor extends Actor{
+      case `execute` =>
+        {
+      //for(i<- 0 to 10000000){
+        			val input = BearerTokenGenerator.getToken()
+        			val hashOutput = BearerTokenGenerator.generateSHAToken(input)
+        			if (Common.isBitCoin(hashOutput,z)){
+        				masterRef! bitcoin(input,hashOutput)
+        				println("bitcoin:"+input+"\thash:"+ hashOutput)
+        			}
+     
+        			masterRef ! interrupt
+        }
+     
+     
+    case `msg` =>
+      println(s"Actor received message")
+      sender ! rmsg
+     
+    case terminate =>
+      {masterRef ! done
+       context.stop(self)
+      //context.system.shutdown()
+      }
+                  
    
-  val remote = context.actorSelection("akka.tcp://MasterSystem@10.136.14.46:2552/user/MiningActor")
-  var counter=0
-  def receive ={
-    case "START" =>
-      		println("Abe hoja")
-      		remote ! "start1"
-    case msg : String=>
-      		println(s"LocalActor received message:'$msg'");
-      		if(counter<5){
-      			sender ! "Hello back to you"
-      			counter += 1
-      		}
-    case start(z) =>
-	    {
-	      for(i<- 0 to 10000000)
-	      {
-	    	  var flag = true
-	    	  val input = BearerTokenGenerator.getToken()
-	    	  val hashOutput = BearerTokenGenerator.generateSHAToken(input)
-	    	  if (Common.isBitCoin(hashOutput,z))
-	    		  println("bitcoin:"+input+"\thash:"+ hashOutput)
-	      }
-	      
-	    }		
   }
   
 }
 
+ class RemoteMiningActor(remoteHost : String) extends Actor{
+   
+  var flag = true 
+  var messageCount=0
+  val masterRef=context.actorSelection(s"akka.tcp://MasterSystem@$remoteHost:2552/user/Master")
+  //println(remoteHost)
+  var counter=0
+  var zeroes=0
+  def receive ={
+      case `execute` =>
+        {
+      //for(i<- 0 to 10000000){
+        			val input = BearerTokenGenerator.getToken()
+        			val hashOutput = BearerTokenGenerator.generateSHAToken(input)
+        			//println("The real zeroes",zeroes)
+        			if (Common.isBitCoin(hashOutput,zeroes)){
+        				masterRef! bitcoin(input,hashOutput)
+        				println("bitcoin:"+input+"\thash:"+ hashOutput)
+        			}
+        			masterRef ! interrupt
+        }
+     
+     
+    case `msg` =>
+      println(s"Actor received message")
+      sender ! rmsg
+     
+    case `terminate` =>
+      {masterRef ! done
+       context.stop(self)
+      //context.system.shutdown()
+      }
+   
+    case `getNoOfZeroes` => masterRef ! sendzeroes        
+    
+    case z:Int => 
+                  { zeroes=z
+                    self!execute 
+                  }
+   
+  }
+  
+}
+ 
+ class Timer(masterRef : ActorRef) extends Actor{
+	var startTime = System.currentTimeMillis()
+	var flag=true
+	while(flag==true)
+	{
+	  var currentTime = System.currentTimeMillis()
+	  
+	  if(currentTime >= startTime+60000)
+	  {
+	    println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+	    flag=false
+	    masterRef ! terminate
+	  }
+	}
+	def receive={
+	  case _=> println("Default")
+	}
+	
+}
